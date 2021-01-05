0670691c9df7d99af93b6688ed8ca66793c0d78b
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-01-05 16:18:18 +0700

    lab8_1completed

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -2,32 +2,48 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main()
+{
+	double loan,interest,amount,rate,newb,total; 
+	int i;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
+	cin >> amount;
+    cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+    newb=loan;
+    
+    for(i=1;i>0;i++)
+    {
+        
+        loan = newb;
+        interest = rate/100*loan;
+	    total = loan+interest; 
+        if(total<amount)
+        {
+            amount = total;
+        }
+        newb = total - amount;
+        if(newb==total) break;
+        
+    
+    cout << fixed << setprecision(2); 
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << amount;
+	cout << setw(13) << left << newb;
 	cout << "\n";	
-	
+    }
 	return 0;
-}
+}     
+    
